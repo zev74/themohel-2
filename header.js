736b71bfdce2ecc3eadd5d372800a64f56ce99d8
zevi <zevishubov@gmail.com>
2021-09-13 18:45:13 -0400

    update routes

--- header.js
+++ header.js
@@ -3,15 +3,15 @@ class Header extends HTMLElement {
     this.innerHTML = ` <nav class="mainNav">
             <ul class="navLinksGroup">
                 <li class="navListItem">
-                    <a class="navLink" href="file:///C:/Users/zevsh/Documents/projects/themohel-2.0/index.html"> Home
+                    <a class="navLink" href="/index.html"> Home
                     </a>
                 </li>
                 <li class="navListItem">
-                    <a class="navLink" href="file:///C:/Users/zevsh/Documents/projects/themohel-2.0/aboutTheMohel.html"> About
+                    <a class="navLink" href="/aboutTheMohel.html"> About
                         The Mohel </a>
                 </li>
                 <li class="navListItem">
-                    <a class="navLink" href="file:///C:/Users/zevsh/Documents/projects/themohel-2.0/aboutTheBris.html"> About
+                    <a class="navLink" href="/aboutTheBris.html"> About
                         The Bris </a>
                 </li>
                 <li class="navListItem"> <a class="navLink"

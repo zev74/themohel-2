0897c182102c824bce7249238f5f5b5fa17487c7
zevi <zevishubov@gmail.com>
2021-09-24 12:48:00 -0400

    add header

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ class Header extends HTMLElement {
   connectedCallback() {
 
     
-    this.innerHTML = ` <div> <nav class="mainNav">
+    this.innerHTML = ` <div> <nav id="mainNav" class="mainNav">
             <ul class="navLinksGroup">
                 <li class="navListItem">
                     <a class="navLink" href="https://zev74.github.io/themohel-2/index.html"> Home
@@ -28,19 +28,32 @@ class Header extends HTMLElement {
         <nav class="mobileNav">
           <h1> hi </h1>
 
-          <div class="hamburger"> 
+          <div id="hamburger" class="hamburger" onClick="toggleMenu"> 
             <label>
-                <input type="checkbox" class="hamburgerCheckbox"> 
+                <input type="checkbox" id="hamburgerCheckbox" class="hamburgerCheckbox"> 
                     <div class="bar1"> </div>
                     <div class="bar2"> </div>
                     <div class="bar3"> </div>
-                </label>
+                </input>
+            </label>
           </div>
 
         </nav>
 
+          <script>
+              const hamburgerCheckbox = document.getElementById('hamburgerCheckbox')
+              const mainNav = document.getElementById('mainNav')
 
-        </div> `
+              const toggleMenu = () => {
+                  hamburgerCheckbox.checked ? mainNav.style.display = "block" :
+                  mainNav.style.display = "none"
+              }
+
+              
+          </script>
+
+
+        </div> ``
         
     }
 }

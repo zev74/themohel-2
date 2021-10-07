335e2e6d9cf3cf96c11633372ce4b3c2c3f2c4b2
zevi <zevishubov@gmail.com>
2021-10-06 23:53:12 -0400

    what pic

--- mobileMenu.js
+++ mobileMenu.js
@@ -3,20 +3,35 @@
 
 const hamburgerCheckbox = document.getElementById('hamburgerCheckbox')
 const hamburger = document.getElementById('hamburger')
-const mainNav = document.getElementById('mainNav')
+const mainNav = document.querySelector('.mainNav')
 
 function toggleMenu() {
     hamburgerCheckbox.checked ? mainNav.style.display = "block" :
     mainNav.style.display = "none"
 }
 
-menuDisplayBlock = () => {
-  if(window.innerWidth > 900) {
-    mainNav.style.display = "block" 
-    console.log(window.innerWidth)
+const menuDisplayBlock = () => {
+
+  let maxQuery = window.matchMedia('(max-width: 900px)')
+  // let minQuery = window.matchMedia('(min-width: 899px)')
+
+  console.log('hi')
+  if(maxQuery.matches) {
+    mainNav.style.display = "flex";
+    mainNav.style.alignItems = "center";
+    mainNav.style.justifyContent = "center";
   }
+  else {
+    mainNav.style.alignItems = "center";
+    mainNav.style.justifyContent = "center";
+  }
+
+  // if(window.innerWidth > 900) {
+  //   mainNav.style.display = "flex" 
+  //   console.log(window.innerWidth)
+  // }
 }
 
 hamburger.addEventListener('click', toggleMenu)
-window.addEventListener('resize', menuDisplayBlock())
+window.addEventListener('resize', menuDisplayBlock)
 
